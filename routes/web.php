2893adb48db2eab3a559c314eb18d7757690eb56
danielsinaga1 <sinagadaniel2@gmail.com>
2022-09-22 10:18:47 +0800

    list of category

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,4 +20,7 @@ Route::get('/', function () {
 
 Auth::routes();
 
+
+
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/category', [App\Http\Controllers\CategoryController::class, 'index'])->name('category.index');
